e111af0a5d76bd209c121e2137115dfc277bb557
Jade Nguyen <jade.nguyen@zoopla.co.uk>
2022-12-22 21:51:13 +0000

    panic function challenge

---
+++ scrimba-challenges/panicFunction.js
@@ -0,0 +1,31 @@
+/* Panic function 
+Write a PANIC! function. The function should take in a sentence and return the same
+sentence in all caps with an exclamation point (!) at the end. Use JavaScript's
+built in string methods. 
+
+If the string is a phrase or sentence, add a 😱 emoji in between each word. 
+
+Example input: "Hello"
+Example output: "HELLO!"
+
+Example input: "I'm almost out of coffee"
+Example output: "I'M 😱 ALMOST 😱 OUT 😱 OF 😱 COFFEE!"
+*/
+
+// Test your function
+// console.log(panic("I'm almost out of coffee"));
+// console.log(panic("winter is coming"))
+
+function panicFunc(input) {
+  let sentence = input.toUpperCase() + "!";
+
+  if (input.includes(" ")) {
+    sentence = sentence.split(" ").join(" 😱 ");
+    // console.log(newSentence)
+  }
+
+  console.log(sentence);
+}
+
+panicFunc("Hello");
+panicFunc("Why are you still awake?");
